32bbb0ef41926b84cc0fe415bc254c06d292bfa9
ankitacode19 <ankitacoder2004@gmail.com>
2020-12-05 22:31:07 +0530

    #26

--- mac/shop/views.py
+++ mac/shop/views.py
@@ -2,4 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("Shop")
+    return render(request, 'shop/index.html')
+
+def about(request):
+    return HttpResponse("WE ARE AT ABOUT.")
+
+def contact(request):
+    return HttpResponse("WE ARE AT CONTACT.")
+
+def tracker(request):
+    return HttpResponse("WE ARE AT TRACKER.")
+
+def search(request):
+    return HttpResponse("WE ARE AT SEARCH.")
+
+def productView(request):
+    return HttpResponse("WE ARE AT PRODUCT VIEW.")
+
+def checkout(request):
+    return HttpResponse("WE ARE AT CHECKOUT.")
